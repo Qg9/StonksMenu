b26760e30f2eaa869aac33a27501308727015daf
Qg9 <gq1792@gmaiL.com>
2025-08-02 17:11:03 +0200

    test version 7

--- src/main/kotlin/fr/qg/menu/models/QGMenu.kt
+++ src/main/kotlin/fr/qg/menu/models/QGMenu.kt
@@ -5,8 +5,8 @@ import fr.qg.menu.actions.ClickScript
 data class QGMenu(
     val title: String,
     val pattern: List<Char>,
-    val items: Map<Char, MenuItem>,
-    val scripts: Map<Char, ClickScript>
+    val items: MutableMap<Char, MenuItem>,
+    val scripts: MutableMap<Char, ClickScript>
 ) {
     val size: Int
         get() = pattern.size
