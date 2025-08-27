8a22e6f4176d61125c341694178534c90b5ac8e8
Qg9 <gq1792@gmaiL.com>
2025-08-27 12:01:06 +0200

    added utility function for faster use

--- common/src/main/kotlin/fr/qg/menu/common/utils/MenuUtils.kt
+++ common/src/main/kotlin/fr/qg/menu/common/utils/MenuUtils.kt
@@ -1,11 +1,27 @@
 package fr.qg.menu.common.utils
 
 import fr.qg.menu.common.models.QGMenu
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
 
 fun QGMenu.mapSlot(char: Char, slot: Int) : Int =
     this.pattern.subList(0, slot).count { it == char }
 
 fun QGMenu.countSlot(char: Char) : Int =
     this.pattern.count { it == char }
+
+fun QGMenu.putInInventory(char: Char, inventory: Inventory, items: List<ItemStack>) : Boolean {
+    val map = this.pattern.withIndex().filter { it.value == char }.map { it.index }
+
+    map.onEachIndexed { ind, slot ->
+        if (ind >= items.size) return false
+        inventory.setItem( slot, items[ind])
+    }
+
+    return true
+}
+
+fun QGMenu.putInInventory(char: Char, inventory: Inventory, item: ItemStack) : Boolean
+    = this.putInInventory(char, inventory, listOf(item))
 
 fun Boolean.toIntMultiplier() = if(this) 1 else -1
